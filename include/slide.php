dea252edccb0023d70255c9742b6d1afe32519c8
anhtu0321 <vantu9486@gmail.com>
2019-12-24 18:20:33 +0700

    hoan thanh slide

--- include/slide.php
+++ include/slide.php
@@ -3,58 +3,70 @@
 	$sql = "select url from anhbanner order by thuTu ASC";
 	$tb = mysql_query($sql);
 ?>
-<div class="container no-padding">
+<div class="container">
 	<div class="row">
-    	<div class="col-sm-12 col-md-12 col-lg-12">
+    	<div class="col-sm-12 col-md-12 col-lg-12 no-padding" id="sliderWraper">
         	<div class="slide">
-           
 				<?  while ($rs = mysql_fetch_array($tb)){?>
                        <img src="<? echo $rs["url"];?>" />
                 <? } ?>     
-            </div>
+			</div>
+			<div class="nuttrai nut" onClick="previous();">&lt;</div>
+    		<div class="nutphai nut" onClick="next();">&gt;</div>
     	</div>
 	</div>
-    <div class="nuttrai" onClick="previous();">&lt;</div>
-    <div class="nutphai" onClick="next();">&gt;</div>
+    
 </div>
 
-<script language="javascript" async>
-/*
-	var img = document.querySelectorAll('.slide img');
-				for(var i =0;i<img.length; i++){
-					img[i].style.width=1000+"px";
-					img[i].style.height=500+"px";	
-				}
-	var W = img.length * 1000;
+<script language="javascript">
+	var sliderWraper = document.getElementById('sliderWraper');
 	var slide = document.getElementsByClassName('slide')[0];
-	if(!slide.style.position) slide.style.position ="absolute";
-	if(!slide.style.left) slide.style.left =0+"px" //Nếu chưa có cái này sẽ không tính được 'x' => lỗi chương trình;
-	slide.style.width = W +"px";
-	var dich =0;
+	var img = document.querySelectorAll('.slide img');
+	var nut = document.getElementsByClassName('nut');
+	var dich, width, W;
+	window.addEventListener('resize',setup);
+	function setup(){
+		dich =0;
+		width = sliderWraper.offsetWidth;
+		W = width*img.length;
+		var height = width/3;
+		sliderWraper.style.height = height +'px';
+		if(!slide.style.position) slide.style.positon = 'absolute';
+		if(!slide.style.left) slide.style.left = 0;
+		for(let i=0;i<img.length;i++){
+			img[i].style.width = width + 'px';
+			img[i].style.height = height + 'px';
+		}
+		slide.style.width = W + 'px';
+		for (let i = 0; i< nut.length; i++){
+			nut[i].style.top = (height/2-10) + 'px';
+		}
+	}
+	setup();
 	slide.auto = setInterval('next()',4000);
 	function next(){
 		if(slide.auto) clearInterval(slide.auto);
-		dich -= 1000;
-		if(dich<1000-W){dich =0;}
+		dich -= width;
+		if(dich<width-W){dich =0;}
 		play();
 		slide.auto = setInterval('next()',4000);
 	}
 	function previous(){
 		if(slide.auto) clearInterval(slide.auto);
-		dich+=1000;
-		if(dich>0){dich=1000-W;}
+		dich+=width;
+		if(dich>0){dich=width-W;}
 		play();
 		slide.auto = setInterval('next()',4000);
 	}
 	function play(){
 		var slide1 = document.getElementsByClassName('slide')[0];
 		var x = parseInt(slide1.style.left);
-		var dif = Math.ceil(Math.abs((dich-x)/10));
+		var dif = Math.ceil(Math.abs((dich-x)/8));
 		if(dich>x){x+=dif;}
 		if(dich<x){x-=dif;}
 		if(dich==x)return;
 		slide1.style.left = x+"px";
 		slide1.move = setTimeout('play()',40);
 	}
-	*/
+	
 </script>
