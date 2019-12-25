fb2908c68824e128a1d085fe36527ff2f29f0f02
hanh <thehanhvu@gmail.com>
2019-12-25 08:48:55 +0700

    Hanh sua

--- include/footer.php
+++ include/footer.php
@@ -1,31 +1,30 @@
 <div class="container-fluid">
-	<div class="row">
-        <div class="col-sm-4 col-md-4 col-lg-4 text-center lienhe">
-            <ul>
-                <li>LIÊN HỆ</li>
-                <li>SHOWROOM YẾN NHI</li>
-                <li><a id="diachi">Đường Dựng - Liên Phương - TP.Hưng Yên</a>̣</li>
-                <li><a id="dtchinh">0975.493.998</a></li>
-                <li><a id="dtkhac">0979.629.930 - 0799.205.666</a></li>
-                <li><a id="mail">nhuonghoang92@gmail.com</a></li>
-            </ul>
+	<div class="row footer">
+        <div class="col-sm-4 col-md-4 col-lg-4 lienhe">
+
+                <h4>LIÊN HỆ</h4>
+                <h4>SHOWROOM YẾN NHI</h4>
+                <p><i class="glyphicon glyphicon-map-marker"></i> Đường Dựng - Liên Phương - TP.Hưng Yên</p>
+                <p><i class="glyphicon glyphicon-phone"></i> 0975.493.998</p>
+                <p><i class="glyphicon glyphicon-phone-alt"></i> 0979.629.930 - 0799.205.666</p>
+                <p><i class="glyphicon glyphicon-envelope"></i> nhuonghoang92@gmail.com</p>
+
         </div>
         
         <div class="col-sm-4 col-md-4 col-lg-4 text-center danhmuc">
-            <ul>
-                <li><a>DANH MỤC</a></li>
+           
+                <h4>DANH MỤC</h4>
                 <? 
                 $sql = "select * from danhmuc where trangThai=1 order by thuTu ASC";
                 $tb = mysql_query($sql);
                 ?>
-                <li>
+                
                 <? 
                 while($rs = mysql_fetch_array($tb)){?>
-                    <li><a href="index.php?xem=danhmuc&iddanhmuc=<? echo $rs["idDanhMuc"];?>"><? echo $rs["tenDanhMuc"];?></a></li>
+                    <p><a href="index.php?xem=danhmuc&iddanhmuc=<? echo $rs["idDanhMuc"];?>"><? echo $rs["tenDanhMuc"];?></a></p>
                 <? }
                 ?>
-                </li>
-            </ul>
+
         </div> 
         <div class="col-sm-4 col-md-4 col-lg-4 text-center facebook"></div>
 	</div>
